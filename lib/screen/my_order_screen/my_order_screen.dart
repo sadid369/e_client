f7030a977cdcf12d7408d63e3472bda84d8750e9
Sadid <66120003+sadid369@users.noreply.github.com>
2024-08-23 16:02:55 +0600

    clean again

--- lib/screen/my_order_screen/my_order_screen.dart
+++ lib/screen/my_order_screen/my_order_screen.dart
@@ -13,7 +13,7 @@ class MyOrderScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //TODO: should complete call getAllOrderByUser
+    context.dataProvider.getAllOrders();
     return Scaffold(
       appBar: AppBar(
         title: const Text(
